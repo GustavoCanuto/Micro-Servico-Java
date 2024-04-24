f187902868e3bda71b993d9bb55f18107ee8a54d
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-04-24 01:29:41 -0300

    first commit

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/dto/StatusDto.java
@@ -0,0 +1,34 @@
+package br.com.alurafood.pedidos.dto;
+
+import br.com.alurafood.pedidos.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusDto {
+    private Status status;
+
+	public StatusDto(Status status) {
+		super();
+		this.status = status;
+	}
+
+	public StatusDto() {
+		super();
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+    
+    
+}
